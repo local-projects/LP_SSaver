d883b8c098493f1e9b0b4df743c342ee268e27ab
Oriol Ferrer Mesià <oriol@uri.cat>
2014-06-06 10:38:45 -0400

    first

---
+++ LPSaver/LPSaverView.h
@@ -0,0 +1,17 @@
+//
+//  LPSaverView.h
+//  LPSaver
+//
+//  Created by Oriol Ferrer Mesià on 29/04/14.
+//  Copyright (c) 2014 Oriol Ferrer Mesià. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+#import <Quartz/Quartz.h>
+#import <QuartzCore/QuartzCore.h>
+
+@interface LPSaverView : ScreenSaverView{
+	QCCompositionLayer * cameraView;
+}
+
+@end
---
+++ LPSaver/LPSaverView.m
@@ -0,0 +1,73 @@
+//
+//  LPSaverView.m
+//  LPSaver
+//
+//  Created by Oriol Ferrer Mesià on 29/04/14.
+//  Copyright (c) 2014 Oriol Ferrer Mesià. All rights reserved.
+//
+
+#import "LPSaverView.h"
+
+@implementation LPSaverView
+
+- (id)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+
+		[self setAnimationTimeInterval:1/60.0];
+		[self setWantsLayer:YES];
+
+		NSString * scriptPath = [[NSBundle bundleForClass:[self class]] pathForResource:@"mountVault" ofType:@"scpt"];
+		NSLog(@"scriptPath: %@", scriptPath);
+		NSArray * args = [NSArray arrayWithObjects: scriptPath, nil];
+		NSTask * task = [NSTask launchedTaskWithLaunchPath: @"/usr/bin/osascript" arguments: args];
+		[task waitUntilExit];
+		[task terminate];
+
+		NSString * compPath = [[NSBundle bundleForClass:[self class]] pathForResource:@"saver" ofType:@"qtz"];
+		NSLog(@"CompPath: %@", compPath);
+
+		frame.origin.x = frame.origin.y = 0.0f;
+
+		NSLog(@"frame: %@", NSStringFromRect(frame));
+		cameraView = [QCCompositionLayer compositionLayerWithFile:compPath];
+		cameraView.frame = NSRectToCGRect(frame);
+		cameraView.opacity = 1.0;
+		[[self layer] addSublayer: cameraView];
+
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
